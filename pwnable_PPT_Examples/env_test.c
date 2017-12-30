cece50127c0c74d96d52f95aa4bf30ecf460ae69
kjkjk <kjkjk1178@gmail.com>
2017-12-30 09:45:02 +0900

    Update env_test.c

--- pwnable_PPT_Examples/env_test.c
+++ pwnable_PPT_Examples/env_test.c
@@ -2,15 +2,15 @@
 int main(int argc, char * argv[], char * envp[])
 {
 	printf("argc 주소 : %p\n", &argc);
-	printf("argv 주소 : %p\n", &argv[0]);
-	printf("envp 주소 : %p\n", &envp[0]);
-	
+	printf("argv 주소 : %p\n", &argv);
+	printf("envp 주소 : %p\n", &envp);
+	printf("envp가 가리키는 배열의 시작 주소 : %p\n", &envp[0]);
+
 	//스택에 저장된 환경변수 접근
 	for(int i =0; envp[i] != NULL; i++)
 	{
-		printf("메인함수 인자 주소 : %p, 저장하는 포인터 값 : %p , 환경변수 값 : %s\n", &envp[i], envp[i], (char*)(envp[i]));
+		printf("배열 주소 : %p, 저장하는 포인터 값 : %p , 환경변수 값 : %s\n", &envp[i], envp[i], (char*)(envp[i]));
 	}
 }
-
 
 
